e9788ae93a38907f359a71af4857a4a26fcf1259
Nick Carver <nicholascarver2@outlook.com>
2021-01-15 10:22:29 -0600

    set up the field.js module

---
+++ src/scripts/field.js
@@ -0,0 +1,16 @@
+plantsGrowing = []
+
+export const addPlant = (seed) => {
+    const checkForArray = Array.isArray(seed)
+    if (checkForArray == true) {
+        plantsGrowing = concat(plantsGrowing, seed)
+    }
+    else {
+        plantsGrowing.push(seed)
+    }
+}
+
+export const usePlants = () => {
+    plantsGrowing.slice()
+}
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,22 @@
 console.log("Welcome to the main module")
 
 import { createPlan } from './plan.js'
+import { createAsparagus } from './seeds/asparagus.js'
+import { createCorn } from './seeds/corn.js'
+import { createPotato } from './seeds/potato.js'
+import { createSoybean } from './seeds/soybean.js'
+import { createSunflower } from './seeds/sunflower.js'
+import { createWheat } from './seeds/wheat.js'
 
 
 const yearlyPlan = createPlan ()
+const asparagus = createAsparagus ()
+const corn = createCorn ()
 
+console.log(yearlyPlan)
+console.log(asparagus)
+console.log(corn)
+console.log(yearlyPlan)
+console.log(yearlyPlan)
+console.log(yearlyPlan)
 console.log(yearlyPlan)
